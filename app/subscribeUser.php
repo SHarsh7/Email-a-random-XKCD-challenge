e0b7eba07c9646084c91936064c22b701b06e051
SHarsh7 <harshsheth9909@gmail.com>
2021-07-05 08:55:25 +0530

    checking decrypted data

--- app/subscribeUser.php
+++ app/subscribeUser.php
@@ -2,15 +2,10 @@
 
 namespace app;
 
-include dirname(__FILE__).'/validateForm.php';
 require_once dirname(__FILE__).'/user.php';
 require_once dirname(__FILE__).'/encdec.php';
 
 if (isset($_GET['activecode'])) {
-        $senitize=new validateForm();
-        //* sanitizing incoming data
-        $code=$senitize->test_input($_GET['activecode']);
-        var_dump($code);
         //*Decrypt the data
         $decryptData=new encdec();
         $code=$decryptData->decrypt($code);

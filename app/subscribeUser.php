4cf8bdf4da2282bc32401444790c746e68b23e79
SHarsh7 <harshsheth9909@gmail.com>
2021-07-12 15:42:31 +0530

    change in message

--- app/subscribeUser.php
+++ app/subscribeUser.php
@@ -24,8 +24,8 @@ if (isset($_GET['activecode'])) {
 
         $user = new user();
         if ($user->updatedata($code)) {
-                echo '<h3>Your account is verified, you will start receiving our emails soon!</h3>
-                <p><b>Note:</b>As we are sending emails in a batch, it may take up to 5-6 minutes to receive the first email so please be patient & Thank you very much for joining us.</p>';
+                echo "<h3>Your account is verified, you will start receiving our emails soon!</h3>
+                <p><b>Note:</b>As we are sending emails in a batch, it may take up to 5-6 minutes to receive the first email, so please be patient & please do check spam and promotions tab if you haven't received any emails in 5-6 minutes & thank you very much for joining us.</p>";
         } else {
                 echo 'something went wrong';
         }

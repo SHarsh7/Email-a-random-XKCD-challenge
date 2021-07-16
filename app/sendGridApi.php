6d056d75c0a237b0f61b9f90864a46d3c2a26173
SHarsh7 <harshsheth9909@gmail.com>
2021-07-16 16:21:03 +0530

    commit

--- app/sendGridApi.php
+++ app/sendGridApi.php
@@ -16,9 +16,6 @@ class sendGridApi
 
         public function __construct()
         {
-        }
-        public function sendVarificationMail($email, $body, $subject)
-        {
                 $sendgrid_apikey = getenv('sendgrid_apikey');
                 $this->session = curl_init($this->url);
                 curl_setopt($this->session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
@@ -26,6 +23,10 @@ class sendGridApi
                 curl_setopt($this->session, CURLOPT_POST, true);
                 curl_setopt($this->session, CURLOPT_HEADER, false);
                 curl_setopt($this->session, CURLOPT_RETURNTRANSFER, true);
+        }
+        public function sendVarificationMail($email, $body, $subject)
+        {       
+              
                 $params = array(
                         'to'        => $email,
                         'from'      => 'noobbot12367@gmail.com',
@@ -33,7 +34,7 @@ class sendGridApi
                         'subject'   => $subject,
                         'html'      => $body,
                         'x-smtpapi' => json_encode($this->js),
-
+                     
                 );
                 curl_setopt($this->session, CURLOPT_POSTFIELDS, $params);
                 $response = curl_exec($this->session);
@@ -48,70 +49,27 @@ class sendGridApi
         }
         public function comicSender($email, $body, $subject, $file)
         {
-                $sendgrid_apikey = getenv('sendgrid_apikey');
+                // $fileName = 'comic.png';
                 $fileName = basename($file);
                 $file = file_get_contents($file);
-
-
-                // $params = array(
-                //         'to'        => $email,
-                //         'from'      => 'noobbot12367@gmail.com',
-                //         'fromname'  => 'XKCD',
-                //         'subject'   => $subject,
-                //         'html'      => $body,
-                //         'x-smtpapi' => json_encode($js),
-                //         // 'files[' . $fileName . ']' => '@' . $filePath . '/' . $fileName,
-                //         'attachments' => array(
-                //                 'content' => 'BASE64_ENCODED_CONTENT',
-                //                 'type' => 'img/png',
-                //                 // 'filename'=>$fileName,
-                //                 'files[' . $fileName . ']' => '@' . $file . '/' . $fileName,
-                //         ),
-                //         // 'type'=> 'image/png',
-                //         'files[' . $fileName . ']' => '@' . $file . '/' . $fileName,
-
-                // );
-                // curl_setopt($this->session, CURLOPT_POSTFIELDS, $params);
-                // $response = curl_exec($this->session);
-                // curl_close($this->session);
-                // return $response ? true : false;
-                $headers = array(
-                        "authorization:'$sendgrid_apikey'",
-                        'Content-Type: application/json'
+               
+                // $filePath = dirname(__FILE__);
+                $params = array(
+                        'to'        => $email,
+                        'from'      => 'noobbot12367@gmail.com',
+                        'fromname'  => 'XKCD',
+                        'subject'   => $subject,
+                        'html'      => $body,
+                        'x-smtpapi' => json_encode($this->js),
+                        // 'files[' . $fileName . ']' => '@' . $filePath . '/' . $fileName,
+                        'type'=> 'image/png',
+                        'files['.$fileName.']' => '@'.$file.'/'.$fileName,
+                       
                 );
-                $data = array(
-                        "personalizations" => array(
-                                array(
-                                        "to" => array(
-                                                array(
-                                                        "email" => $email
-                                                )
-                                        )
-                                )
-                                                ),
-                        "from"=>array(
-                                "email"=>"noobbot12367@gmail.com"
-                        ),
-                        "subject"=>$subject,
-                        "content"=>array(
-                                array(
-                                        "type"=>"text/html",
-                                        "value"=> $body
-                                )
-                        )
-                );
-
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/v3/mail/send');
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                $response = curl_exec($ch);
-                echo $response;
-                curl_close($ch);
-
-                // unlink($file);
+                curl_setopt($this->session, CURLOPT_POSTFIELDS, $params);
+                $response = curl_exec($this->session);
+                curl_close($this->session);
+                unlink($file);
+                return $response ? true : false;
         }
 }

e8abc8cad514919ec8baf5f79fe0956b5a282d51
SHarsh7 <harshsheth9909@gmail.com>
2021-07-16 16:08:19 +0530

    echo

--- app/sendGridApi.php
+++ app/sendGridApi.php
@@ -109,6 +109,7 @@ class sendGridApi
                 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 $response = curl_exec($ch);
+                echo $response;
                 curl_close($ch);
 
                 // unlink($file);
